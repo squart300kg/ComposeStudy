488f21bb0cbe8241cdd8488a8fd88ba2df053079
n22301003-sysong <sysong@pentasecurity.com>
2023-03-01 18:01:11 +0900

    [basicStateCodelab] 상태에 따른 UI설정

--- BasicStateCodelab/app/src/main/java/com/codelabs/state/ui/theme/WellnessScreen.kt
+++ BasicStateCodelab/app/src/main/java/com/codelabs/state/ui/theme/WellnessScreen.kt
@@ -17,8 +17,14 @@ fun WellnessScreen(modifier: Modifier = Modifier) {
 fun WaterCounter(modifier: Modifier = Modifier) {
     Column(modifier = modifier.padding(16.dp)) {
         var count by remember { mutableStateOf(0) }
-        Text("You've had $count gladdes.")
-        Button(onClick = { count++ }, Modifier.padding(top = 8.dp)) {
+        if (count > 0){
+            Text("You've had $count gladdes.")
+        }
+        Button(
+            onClick = { count++ },
+            modifier = Modifier.padding(top = 8.dp),
+            enabled = count < 10
+        ) {
             Text("Add one")
         }
     }
