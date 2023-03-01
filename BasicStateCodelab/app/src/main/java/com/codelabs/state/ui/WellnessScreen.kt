280906ff7a4b6cbb6d451d363fc4fd95a825f7f9
n22301003-sysong <sysong@pentasecurity.com>
2023-03-01 18:37:25 +0900

    [basicStateCodelab] RememberSaveable 적용

--- BasicStateCodelab/app/src/main/java/com/codelabs/state/ui/WellnessScreen.kt
+++ BasicStateCodelab/app/src/main/java/com/codelabs/state/ui/WellnessScreen.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -19,29 +20,16 @@ fun WellnessScreen(modifier: Modifier = Modifier) {
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
     Column(modifier = modifier.padding(16.dp)) {
-        var count by remember { mutableStateOf(0) }
-        if (count > 0){
-            var showTask by remember { mutableStateOf(true) }
-            if (showTask) {
-                WellnessTaskItem(
-                    onClose = { showTask = false },
-                    taskName = "Have you taken your 15 minute walk today?"
-                )
-            }
-            Text("You've had $count gladdes.")
+        var count by rememberSaveable { mutableStateOf(0) }
+        if (count > 0) {
+            Text("You've had $count glasses.")
         }
-        Row(
+        Button(
+            onClick = { count++ },
             modifier = Modifier.padding(top = 8.dp),
+            enabled = count < 10
         ) {
-            Button(
-                onClick = { count++ },
-                enabled = count < 10
-            ) {
-                Text("Add one")
-            }
-            Button(onClick = { count = 0 }, modifier = Modifier.padding(start = 8.dp)) {
-                Text("Clear water count")
-            }
+            Text("Add on")
         }
     }
 }
