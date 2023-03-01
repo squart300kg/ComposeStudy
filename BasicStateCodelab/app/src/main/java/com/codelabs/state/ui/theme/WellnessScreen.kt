94792d7b62785bc661bf44d08a9af8c7130875d0
n22301003-sysong <sysong@pentasecurity.com>
2023-03-01 17:37:19 +0900

    [basicStateCodelab] WaterCounter정의

---
+++ BasicStateCodelab/app/src/main/java/com/codelabs/state/ui/theme/WellnessScreen.kt
@@ -0,0 +1,25 @@
+package com.codelabs.state.ui.theme
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun WellnessScreen(modifier: Modifier = Modifier) {
+    WaterCounter(modifier)
+}
+
+@Composable
+fun WaterCounter(modifier: Modifier = Modifier) {
+    Column(modifier = modifier.padding(16.dp)) {
+        var count = 0
+        Text("You've had $count gladdes.")
+        Button(onClick = { count++ }, Modifier.padding(top = 8.dp)) {
+            Text("Add one")
+        }
+    }
+}
