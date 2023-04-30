189bd27948ee858849d178da5e55da05c599a7ab
n22301003-sysong <sysong@pentasecurity.com>
2023-05-01 02:13:20 +0900

    navHost추출

--- NavigationCodelab/app/src/main/java/com/example/compose/rally/RallyActivity.kt
+++ NavigationCodelab/app/src/main/java/com/example/compose/rally/RallyActivity.kt
@@ -29,20 +29,19 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.navigation.NavGraph
+import androidx.navigation.*
 import androidx.navigation.NavGraph.Companion.findStartDestination
-import androidx.navigation.NavHostController
-import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
-import androidx.navigation.navArgument
 import com.example.compose.rally.data.Bill
+import com.example.compose.rally.ui.RallyNavHost
 import com.example.compose.rally.ui.accounts.AccountsScreen
 import com.example.compose.rally.ui.accounts.SingleAccountScreen
 import com.example.compose.rally.ui.bills.BillsScreen
 import com.example.compose.rally.ui.components.RallyTabRow
+import com.example.compose.rally.ui.navigateSingleTopTo
 import com.example.compose.rally.ui.overview.OverviewScreen
 import com.example.compose.rally.ui.theme.RallyTheme
 
@@ -73,68 +72,16 @@ fun RallyApp() {
                 RallyTabRow(
                     allScreens = rallyTabRowScreens,
                     onTabSelected = { newScreen ->
-                        navController.navigationSingleTopTo(newScreen.route)
+                        navController.navigateSingleTopTo(newScreen.route)
                     },
                     currentScreen = currentScreen
                 )
             }
         ) { innerPadding ->
-            NavHost(
+            RallyNavHost(
                 navController = navController,
-                startDestination = Overview.route,
                 modifier = Modifier.padding(innerPadding)
-            ) {
-                composable(route = Overview.route) {
-                    OverviewScreen(
-                        onClickSeeAllAccounts = {
-                            navController.navigationSingleTopTo(Accounts.route)
-                        },
-                        onClickSeeAllBills = {
-                            navController.navigationSingleTopTo(Bills.route)
-                        },
-                        onAccountClick = { accountType ->
-                            Log.i("singleAccountLog", "2 $accountType")
-                            navController.navigateToSingleAccount(accountType)
-                        }
-                    )
-                }
-                composable(route = Accounts.route) {
-                    AccountsScreen(
-                        onAccountClick = { accountType ->
-                            Log.i("singleAccountLog", "3 $accountType")
-                            navController.navigateToSingleAccount(accountType)
-                        }
-                    )
-                }
-                composable(route = Bills.route) {
-                    BillsScreen()
-                }
-                composable(
-                    route = SingleAccount.routeWithArgs,
-                    arguments = SingleAccount.arguments
-                ) { navBackStackEntry ->
-                    val accountType =
-                        navBackStackEntry.arguments?.getString(SingleAccount.accountTypeArg)
-                    Log.i("singleAccountLog", "1 $accountType")
-                    SingleAccountScreen(accountType)
-                }
-            }
+            )
         }
     }
 }
-
-private fun NavHostController.navigateToSingleAccount(accountType: String) {
-    Log.i("singleAccountLog", "0 $accountType")
-    this.navigationSingleTopTo("${SingleAccount.route}/$accountType")
-}
-
-fun NavHostController.navigationSingleTopTo(route: String) =
-    this.navigate(route) {
-        popUpTo(
-            this@navigationSingleTopTo.graph.findStartDestination().id
-        ) {
-            saveState = true
-        }
-        launchSingleTop = true
-        restoreState = true
-    }
